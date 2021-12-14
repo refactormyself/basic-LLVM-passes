83f258b0b5d1c0947692017718f238b40f32aa99
Bolarinwa O. Saheed <refactormyself@gmail.com>
2021-12-14 08:49:10 +0100

    Add stub include folder

---
+++ include/Passes.hpp
@@ -0,0 +1,10 @@
+#ifndef LLVM_NEW_PASSES_PASSES_H
+#define LLVM_NEW_PASSES_PASSES_H
+
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+
+
+#endif // LLVM_NEW_PASSES_PASSES_H
